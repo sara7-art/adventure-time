6dca1c9d6458fa12b949bcb40bb6905221c93e04
Sara Yoda <yodasara700@gmail.com>
2026-04-22 12:37:44 -0700

    initial commit

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,7 +1,34 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class AdventureTime {
-    public static void main(){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        loadAdventure();
+    }
+
+    public static void loadAdventure() {
+
+
+        try {
+            FileReader fileReader = new FileReader("adventure1.csv");
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = reader.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Error: File not found or cannot be read.");
+        }
 
     }
 }
